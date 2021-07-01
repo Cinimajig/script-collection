9e40d6dd25dcf9c8b654762b0e059b898f436ade
Cinimajig <39052490+Cinimajig@users.noreply.github.com>
2021-07-01 09:28:22 +0200

    Added architecture variable

--- Install-Python-64.cmd
+++ Install-Python-64.cmd
@@ -1,17 +1,18 @@
 @ECHO OFF
 
-REM Downloads and installs Python. Modify PYTHON_VERSION and install parameters to fit your needs.
+REM Downloads and installs Python. Modify PYTHON_VERSION, ARCH and install parameters to fit your needs.
 
 SET PYTHON_VERSION=3.9.6
+SET ARCH=amd64
 
 CD "%~dp0"
 
 IF NOT EXIST python-%PYTHON_VERSION%-amd64.exe (
   ECHO Installer not found for version %PYTHON_VERSION%. Donwloading from python.org...
 
-  powershell.exe -NoProfile -Command "Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/%PYTHON_VERSION%/python-%PYTHON_VERSION%-amd64.exe' -OutFile 'python-%PYTHON_VERSION%-amd64.exe'"
+  powershell.exe -NoProfile -Command "Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/%PYTHON_VERSION%/python-%PYTHON_VERSION%-%ARCH%.exe' -OutFile 'python-%PYTHON_VERSION%-%ARCH%.exe'"
   TIMEOUT 1 /NOBREAK
 )
 
 ECHO Installing Python %PYTHON_VERSION%...
-python-%PYTHON_VERSION%-amd64.exe /passive InstallAllUsers=1 CompileAll=1 PrependPath=1 
+python-%PYTHON_VERSION%-%ARCH%.exe /passive InstallAllUsers=1 CompileAll=1 PrependPath=1 
